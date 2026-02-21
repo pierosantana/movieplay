a602ac312f2c86496dc4044ef94232d2be399feb
pierosantana <piero.santanalarosa@gmail.com>
2026-02-21 19:35:19 +0100

    feat: crear CrudMovieRepository con Spring Data y agregar data.sql

---
+++ src/main/java/com/psantana/movieplay/persistence/crud/CrudMovieEntity.java
@@ -0,0 +1,7 @@
+package com.psantana.movieplay.persistence.crud;
+
+import com.psantana.movieplay.persistence.entity.MovieEntity;
+import org.springframework.data.repository.CrudRepository;
+
+public interface CrudMovieEntity extends CrudRepository<MovieEntity, Long> {
+}
